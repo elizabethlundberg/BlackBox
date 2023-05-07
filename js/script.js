8f128f2bfeeab88683278d436f121611fc568aa8
Lark Lundberg <samflundberg@gmail.com>
2023-05-07 14:40:59 -0500

    Finish for today, working on edge detection

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ let cellState = {
   A: 'white'
 }
 
+let DIRECTIONS = {
+  0: 'u',
+  1: 'r',
+  2: 'd',
+  3: 'l'
+}
+
 /*----- state variables -----*/
 let board = [
   [0, 0, 0, 0, 0],
@@ -21,59 +28,178 @@ const raySelEls = document.querySelectorAll('.ray-selector')
 const newGameButton = document.querySelector('button')
 
 /*----- functions -----*/
+const determineMove = (test1, test2, test3) => {
+  if (test2 === 'ATOM') {
+    return 'HIT'
+  } else if (test1 === 'ATOM' && test3 === 'ATOM') {
+    return '180'
+  } else if (test1 === 'ATOM') {
+    return 'BEND FROM 1'
+  } else if (test3 === 'ATOM') {
+    return 'BEND FROM 3'
+  } else {
+    return 'CLEAR'
+  }
+}
+
+const takeStep = (electronRow, electronCol, electronDir) => {
+  let exitStatus = false
+  if (electronDir === 0) {
+    if (electronRow === 0) {
+      exitStatus = true
+      return
+    } else {
+      electronRow--
+    }
+  } else if (electronDir === 1) {
+    if (electronCol === board.length) {
+      exitStatus = true
+      return
+    } else {
+      electronCol++
+    }
+  } else if (electronDir === 2) {
+    if (electronRow === board.length) {
+      exitStatus = true
+      return
+    } else {
+      electronRow++
+    }
+  } else if (electronDir === 3) {
+    if (electronCol === 0) {
+      exitStatus = true
+      return
+    } else {
+      electronCol--
+    }
+  }
+  console.log(exitStatus)
+  return [electronRow, electronCol, exitStatus]
+}
+
 const handleCheckCell = (checkRow, checkCol) => {
   if (board[checkRow][checkCol] === 'A') {
-    return 'RETURN'
+    return 'ATOM'
   }
 }
 
-const handleEmission = (electronRow, electronCol, electronDir) => {
-  if (board[electronRow][electronCol] === 'A') {
+const handleCheckStep = (electronRow, electronCol, electronDir) => {
+  console.log(`${electronRow}, ${electronCol}, ${electronDir}`)
+  let test1
+  let test2
+  let test3
+  let nextRow
+  let nextCol
+  if (electronDir === 0) {
+    test1 = handleCheckCell(electronRow - 1, electronCol - 1)
+    test2 = handleCheckCell(electronRow - 1, electronCol)
+    test3 = handleCheckCell(electronRow - 1, electronCol + 1)
+    nextStep = determineMove(test1, test2, test3)
+  } else if (electronDir === 2) {
+    test1 = handleCheckCell(electronRow + 1, electronCol + 1)
+    test2 = handleCheckCell(electronRow + 1, electronCol)
+    test3 = handleCheckCell(electronRow + 1, electronCol - 1)
+    nextStep = determineMove(test1, test2, test3)
+  } else if (electronDir === 3) {
+    if (electronRow === 0) {
+      test1 = handleCheckCell(electronRow + 1, electronCol - 1)
+      test2 = handleCheckCell(electronRow, electronCol - 1)
+      console.log(determineMove(test1, test2, test3))
+    } else if (electronRow === board.length - 1) {
+      test2 = handleCheckCell(electronRow, electronCol - 1)
+      test3 = handleCheckCell(electronRow - 1, electronCol - 1)
+      nextStep = determineMove(test1, test2, test3)
+    } else {
+      test1 = handleCheckCell(electronRow - 1, electronCol - 1)
+      test2 = handleCheckCell(electronRow, electronCol - 1)
+      test3 = handleCheckCell(electronRow + 1, electronCol - 1)
+      nextStep = determineMove(test1, test2, test3)
+    }
+  } else if (electronDir === 1) {
+    if (electronRow === 0) {
+      test2 = handleCheckCell(electronRow, electronCol - 1)
+      test3 = handleCheckCell(electronRow + 1, electronCol - 1)
+      nextStep = determineMove(test1, test2, test3)
+    } else if (electronRow === board.length - 1) {
+      test1 = handleCheckCell(electronRow - 1, electronCol - 1)
+      test2 = handleCheckCell(electronRow, electronCol - 1)
+      nextStep = determineMove(test1, test2, test3)
+    } else {
+      test1 = handleCheckCell(electronRow - 1, electronCol + 1)
+      test2 = handleCheckCell(electronRow, electronCol + 1)
+      test3 = handleCheckCell(electronRow + 1, electronCol + 1)
+      nextStep = determineMove(test1, test2, test3)
+    }
+  }
+  if (nextStep === 'HIT') {
     console.log('HIT')
     return
-  }
-  if (electronDir === 'u' || electronDir === 'd') {
-    let result1 = handleCheckCell(electronRow, electronCol - 1)
-    let result2 = handleCheckCell(electronRow, electronCol + 1)
-    if (result1 === 'RETURN' || result2 === 'RETURN') {
-      console.log('RETURN')
-    } else {
-      console.log('CLEAR PATH')
+  } else if (nextStep === 'BEND FROM 1') {
+    electronDir++
+  } else if (nextStep === 'BEND FROM 3') {
+    electronDir--
+  } else if (nextStep === '180') {
+    electronDir += 2
+  } else if (nextStep === 'CLEAR') {
+    console.log('CLEAR')
+    let nextStepLoc = takeStep(electronRow, electronCol, electronDir)
+    if (nextStepLoc[2] === true) {
+      console.log('EXITED')
+      return
     }
+    nextRow = nextStepLoc[0]
+    nextCol = nextStepLoc[1]
   }
-  if (electronDir === 'l' || electronDir === 'r') {
-    let result1
-    let result2
-    if (electronRow === 0) {
-      result2 = handleCheckCell(electronRow + 1, electronCol)
-    } else if (electronRow === board.length - 1) {
-      result1 = handleCheckCell(electronRow - 1, electronCol)
-    } else {
-      result1 = handleCheckCell(electronRow - 1, electronCol)
-      result2 = handleCheckCell(electronRow + 1, electronCol)
-    }
-    if (result1 === 'RETURN' || result2 === 'RETURN') {
-      console.log('RETURN')
-    } else {
-      console.log('CLEAR PATH')
-    }
-  }
+  console.log(nextStep)
+  handleCheckStep(nextRow, nextCol, electronDir)
 }
 
-const ponderAdjacent = (electronRow, electronCol, electronDir) => {
+// handleEmission handles the first step, which has different rules, then calls handleStep, which runs recursively until it hits the edge.
+const handleEmission = (electronRow, electronCol, electronDir) => {
+  let status
+  let nextCell
   if (board[electronRow][electronCol] === 'A') {
-    console.log('HIT')
-  }
-  if (electronDir === 'u' || electronDir === 'd') {
-    console.log('Got here')
-    let adjCol1 = electronCol - 1
-    let adjCol2 = electronCol + 1
-    if (board[electronRow][adjCol1] === 'A') {
-      console.log('RETURN')
+    status = 'HIT'
+  } else {
+    if (electronDir === 0 || electronDir === 2) {
+      let result1 = handleCheckCell(electronRow, electronCol - 1)
+      let result2 = handleCheckCell(electronRow, electronCol + 1)
+      if (result1 === 'ATOM' || result2 === 'ATOM') {
+        status = 'RETURN'
+      } else {
+        status = 'CLEAR'
+      }
+    } else if (electronDir === 3 || electronDir === 1) {
+      let result1
+      let result2
+      if (electronRow === 0) {
+        result2 = handleCheckCell(electronRow + 1, electronCol)
+      } else if (electronRow === board.length - 1) {
+        result1 = handleCheckCell(electronRow - 1, electronCol)
+      } else {
+        result1 = handleCheckCell(electronRow - 1, electronCol)
+        result2 = handleCheckCell(electronRow + 1, electronCol)
+      }
+      if (result1 === 'ATOM' || result2 === 'ATOM') {
+        status = 'RETURN'
+      } else {
+        status = 'CLEAR'
+      }
     }
+  }
+  if (status === 'HIT') {
+    console.log(status)
+    return
+  } else if (status === 'RETURN') {
+    console.log(status)
+    return
+  } else if (status === 'CLEAR') {
+    nextCell = takeStep(electronRow, electronCol)
+    handleCheckStep(nextCell[0], nextCell[1], electronDir)
   }
 }
 
+// The ray click determines what cell to start on, then passes it on to the HandleEmission function.
 const handleRayClick = (selector) => {
   let electronRow
   let electronCol
@@ -81,19 +207,19 @@ const handleRayClick = (selector) => {
   if (selector.includes('top')) {
     electronRow = 0
     electronCol = Number(selector.charAt(selector.length - 1))
-    electronDir = 'd'
+    electronDir = 2
   } else if (selector.includes('bottom')) {
     electronRow = board.length - 1
     electronCol = Number(selector.charAt(selector.length - 1))
-    electronDir = 'u'
+    electronDir = 0
   } else if (selector.includes('left')) {
     electronCol = 0
     electronRow = Number(selector.charAt(selector.length - 1))
-    electronDir = 'r'
+    electronDir = 1
   } else if (selector.includes('right')) {
     electronCol = board.length - 1
     electronRow = Number(selector.charAt(selector.length - 1))
-    electronDir = 'l'
+    electronDir = 3
   }
   handleEmission(electronRow, electronCol, electronDir)
 }
@@ -112,7 +238,6 @@ const handleClick = (evt) => {
   if (evt.target.className === 'ray-selector') {
     handleRayClick(evt.target.id)
   } else if (evt.target.className === 'cell') {
-    console.log(evt.target.id)
   }
 }
 
